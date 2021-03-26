4de8939f56acf22fc4454852d3960cf4ebad4a5c
EdgarOrtiz193241146 <81432925+EdgarOrtiz193241146@users.noreply.github.com>
2021-03-26 10:37:58 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyA2WjaW0IlN7XKYb60RN8A8QFca_6lHQ0I",
+    authDomain: "chat43eaot.firebaseapp.com",
+    projectId: "chat43eaot",
+    storageBucket: "chat43eaot.appspot.com",
+    messagingSenderId: "968319540062",
+    appId: "1:968319540062:web:5992b49c57f8c318030dcf",
+    measurementId: "G-9H5Y7XV9QN"
+    });
